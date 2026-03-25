02b8ea20291f3b8d7c3eecc1252ae40d1f6724f0
Sujal Makwana <114184382+Maksuzs2401@users.noreply.github.com>
2026-03-26 00:50:47 +0530

    neuron.sv

--- neuron.sv
+++ neuron.sv
@@ -29,13 +29,15 @@ input logic signed  [data_width-1:0] my_weight,
 input logic                          my_input_valid,
 output logic signed [accu_width-1:0] out 
     );
+    //==== Internal wires ====//
   logic signed [data_width-1:0] input_delay;
   logic signed [data_width-1:0] weight_delay;
   logic signed [(data_width*2)-1:0] mul_result;
   logic signed [accu_width-1:0]sum_result;
   logic clear_d;
   logic valid_d;
-  
+
+    //==== D flip flop acting as a buffer/delay unit ====//
   always_ff @(posedge clk or negedge rst_n)begin
     if(!rst_n)begin
       input_delay <=0;
@@ -49,11 +51,13 @@ output logic signed [accu_width-1:0] out
       weight_delay <= my_weight;
     end
   end
-  
+
+    //==== Combinational block for multiplication unit ====//
   always_comb begin
     mul_result = input_delay * weight_delay;
   end
 
+    //==== Summation unit ====//
   always @(posedge clk or negedge rst_n)begin
     if(!rst_n)
       sum_result <=0;
@@ -64,7 +68,8 @@ output logic signed [accu_width-1:0] out
         sum_result <= sum_result + mul_result;
     end
   end
-  
+
+    //==== ReLU activation unit ====//
   assign out = (sum_result[accu_width-1]==1'b1) ? '0:sum_result;
   
 endmodule
