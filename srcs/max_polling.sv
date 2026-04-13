44c04f45d72c35fde161a396af26daabf0432760
Sujal Makwana <114184382+Maksuzs2401@users.noreply.github.com>
2026-04-13 14:00:04 +0530

    Update max_polling.sv

--- srcs/max_polling.sv
+++ srcs/max_polling.sv
@@ -1,37 +1,16 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 29.03.2026 16:27:00
-// Design Name: 
-// Module Name: max_polling
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 `include "config.vh"
 
 module max_polling #(parameter IN_CHANNELS = 32)(
     input  logic                         clk,
     input  logic                         rst_n,
     input  logic                         s_axis_tvalid,
-    //input  logic                         s_axis_tlast,
     output logic                         s_axis_tready,
     input  logic signed [`accu_width-1:0]s_axis_tdata[IN_CHANNELS-1:0],
     
     output logic signed [`accu_width-1:0]m_axis_tdata[IN_CHANNELS-1:0],
     output logic                         m_axis_tvalid,
     input  logic                         m_axis_tready
-    //output logic                         m_axis_tlast
     );
     
     logic toggle_state;
@@ -43,19 +22,16 @@ module max_polling #(parameter IN_CHANNELS = 32)(
     if (!rst_n) begin
         toggle_state  <= 1'b0;
         m_axis_tvalid <= 1'b0;
-        //m_axis_tlast  <= 1'b0;
+        
         for (int i = 0; i < IN_CHANNELS; i++) begin
             temp_reg[i]    <= 0;
             m_axis_tdata[i]<= 0;
         end
     end else begin
-        // Clear valid after downstream handshake
         if (m_axis_tvalid && m_axis_tready)begin
             m_axis_tvalid <= 1'b0;
-           // m_axis_tlast  <= 1'b0;
         end
-        // Accept new upstream data
-        if (s_axis_tvalid && s_axis_tready) begin
+        if (s_axis_tvalid && s_axis_tready) begin     // Accept new upstream data
             if (toggle_state == 1'b0) begin
                 // FIRST sample of pair: just store, do NOT assert valid yet
                 for (int i = 0; i < IN_CHANNELS; i++)
@@ -67,7 +43,6 @@ module max_polling #(parameter IN_CHANNELS = 32)(
                     m_axis_tdata[i] <= (s_axis_tdata[i] > temp_reg[i])
                                         ? s_axis_tdata[i] : temp_reg[i];
                 toggle_state  <= 1'b0;
-              //  m_axis_tlast <= s_axis_tlast;
                 m_axis_tvalid <= 1'b1;
             end
         end
